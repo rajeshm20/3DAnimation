51b0b33639cd4d8b01066eb6cd0e3e5abba9ecf9
Rajesh M <rajeshm@rajeshs-mac.local>
2016-12-12 12:29:44 +0530

    Initial Commit

---
+++ 3DAnimationTests/_DAnimationTests.swift
@@ -0,0 +1,36 @@
+//
+//  _DAnimationTests.swift
+//  3DAnimationTests
+//
+//  Created by Rajesh M on 12/12/16.
+//  Copyright © 2016 sasvathtech. All rights reserved.
+//
+
+import XCTest
+@testable import _DAnimation
+
+class _DAnimationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
